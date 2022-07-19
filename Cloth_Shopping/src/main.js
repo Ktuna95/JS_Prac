8bdb189beca5b57ce3e594bc26f7be138ecb38be
Ktuna <62793010+Ktuna95@users.noreply.github.com>
2022-07-19 15:53:27 +0900

    Laptop

--- Cloth_Shopping/src/main.js
+++ Cloth_Shopping/src/main.js
@@ -1,14 +1,6 @@
-const target = document.querySelector('.target');
-const links = document.querySelectAll('.content .item');
 
-links.addEventListener("mouseover", function zoomIn(links) {
-    links.target.style.transform = "scale(1.2)";
-    links.target.style.zIndex = 1;
-    links.target.style.transition = "all 0.5s";
-  });
-
-links.addEventListener("mousedown", function zoomOut(links) {
-    links.target.style.transform = "scale(1)";
-    links.target.style.zIndex = 0;
-    links.target.style.transition = "all 0.5s";
-  });  
+/*Click 메서드 이벤트*/
+const click = document.querySelector('.click');  
+var yellow [];
+  var blue [];
+  var pink [];
